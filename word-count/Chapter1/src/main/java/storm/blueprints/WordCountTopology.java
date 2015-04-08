d4c485cd3e9e046fc4385beb3c36b394b5eb139c
Radhwane Chebaane <radcheb@gmail.com>
2015-04-08 15:01:40 +0200

    Exploiting Storm parallelism

--- word-count/Chapter1/src/main/java/storm/blueprints/WordCountTopology.java
+++ word-count/Chapter1/src/main/java/storm/blueprints/WordCountTopology.java
@@ -22,15 +22,16 @@ public class WordCountTopology {
     	
     	TopologyBuilder builder = new TopologyBuilder();
     	
-    	builder.setSpout(SENTENCE_SPOUT_ID, spout);
+    	builder.setSpout(SENTENCE_SPOUT_ID, spout,2);
     	// sentenceSpout -->  splitSentenceBolt
-    	builder.setBolt(SPLIT_BOLT_ID, splitBolt).shuffleGrouping(SENTENCE_SPOUT_ID);
+    	builder.setBolt(SPLIT_BOLT_ID, splitBolt,2).setNumTasks(4).shuffleGrouping(SENTENCE_SPOUT_ID);
     	// splitSentenceBolt --> wordCountBolt
-    	builder.setBolt(COUNT_BOLT_ID, countBolt).fieldsGrouping(SPLIT_BOLT_ID,new Fields("word"));
+    	builder.setBolt(COUNT_BOLT_ID, countBolt,4).fieldsGrouping(SPLIT_BOLT_ID,new Fields("word"));
     	// wordCountBolt --> reportBolt
     	builder.setBolt(REPORT_BOLT_ID, reportBolt).globalGrouping(COUNT_BOLT_ID);
     	
     	Config config = new Config();
+    	config.setNumWorkers(2);
     	
     	LocalCluster cluster = new LocalCluster();
     	
