429a78ff11d5c0db3f2715dfacedc55c5f549e3f
Radhwane Chebaane <radcheb@gmail.com>
2015-04-08 13:33:52 +0200

    word count v1

---
+++ word-count/Chapter1/src/main/java/storm/blueprints/SentenceSpout.java
@@ -0,0 +1,52 @@
+package storm.blueprints;
+
+import java.util.Map;
+
+import storm.blueprints.utils.Utils;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+
+
+public class SentenceSpout extends BaseRichSpout {
+
+	private SpoutOutputCollector collector;
+
+	private String[] sentences = { "my dog has fleas", "i like cold beverages",
+			"the dog ate my homework", "don't have a cow man",
+			"i don't think i like fleas" };
+
+	private int index = 0;
+	
+	/*
+	 * Called to request next tuple in the collector
+	 * @see backtype.storm.spout.ISpout#nextTuple()
+	 */
+	public void nextTuple() {
+		
+		this.collector.emit(new Values(sentences[index%sentences.length]));
+		index++;
+		Utils.waitForMillis(1);
+	}
+	/*
+	 * from the ISpout interface and is called on the spout initialization
+	 * map : contain storm configuration
+	 * context: contain topology components
+	 * @see backtype.storm.spout.ISpout#open(java.util.Map, backtype.storm.task.TopologyContext, backtype.storm.spout.SpoutOutputCollector)
+	 */
+	public void open(Map arg0, TopologyContext arg1, SpoutOutputCollector outputCollector) {
+		
+		this.collector = outputCollector;
+	}
+	/*
+	 * from the IComponent interface; declare the type out the output streams
+	 * @see backtype.storm.topology.IComponent#declareOutputFields(backtype.storm.topology.OutputFieldsDeclarer)
+	 */
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("sentence"));
+	}
+
+}
---
+++ word-count/Chapter1/src/main/java/storm/blueprints/WordCountTopology.java
@@ -0,0 +1,44 @@
+package storm.blueprints;
+
+import storm.blueprints.utils.Utils;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+
+public class WordCountTopology {
+
+    private static final String SENTENCE_SPOUT_ID = "sentence-spout";
+    private static final String SPLIT_BOLT_ID = "split-bolt";
+    private static final String COUNT_BOLT_ID = "count-bolt";
+    private static final String REPORT_BOLT_ID = "report-bolt";
+    private static final String TOPOLOGY_NAME = "word-count-topology";
+
+    public static void main(String[] args) throws Exception{
+    	SentenceSpout spout= new SentenceSpout();
+    	SplitSentenceBolt splitBolt = new SplitSentenceBolt();
+    	WordCountBolt countBolt = new WordCountBolt();
+    	ReportBolt reportBolt = new ReportBolt();
+    	
+    	TopologyBuilder builder = new TopologyBuilder();
+    	
+    	builder.setSpout(SENTENCE_SPOUT_ID, spout);
+    	// sentenceSpout -->  splitSentenceBolt
+    	builder.setBolt(SPLIT_BOLT_ID, splitBolt).shuffleGrouping(SENTENCE_SPOUT_ID);
+    	// splitSentenceBolt --> wordCountBolt
+    	builder.setBolt(COUNT_BOLT_ID, countBolt).fieldsGrouping(SPLIT_BOLT_ID,new Fields("word"));
+    	// wordCountBolt --> reportBolt
+    	builder.setBolt(REPORT_BOLT_ID, reportBolt).globalGrouping(COUNT_BOLT_ID);
+    	
+    	Config config = new Config();
+    	
+    	LocalCluster cluster = new LocalCluster();
+    	
+    	cluster.submitTopology(TOPOLOGY_NAME, config, builder.createTopology());
+    	
+    	Utils.waitForSeconds(10);
+    	
+    	cluster.killTopology(TOPOLOGY_NAME);
+    	cluster.shutdown();
+    }
+}
